8b41714d2ce6a27a284576f2971c9c356ac07078
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-07-26 21:51:24 +0300

    feat(integrations): use another color for incline highlight

--- lua/obscure/groups/integrations/incline.lua
+++ lua/obscure/groups/integrations/incline.lua
@@ -1,6 +1,6 @@
 local p = require("obscure.palette")
 
 return {
-  IncLineNormal = { fg = p.fg, bg = p.gray4, bold = true },
+  IncLineNormal = { fg = p.fg, bg = p.gray2, bold = true },
   IncLineNormalNC = { fg = p.subtext3, bg = p.gray1, bold = true },
 }
