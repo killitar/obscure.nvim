23e0772a28c4607d82e4da48e5d52b04a3167e12
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-08-25 18:13:13 +0300

    style(pallete): remove unused comment

--- lua/obscure/palette/init.lua
+++ lua/obscure/palette/init.lua
@@ -4,8 +4,6 @@ function M.setup()
   local type_pallete = "obscure"
   local _, palette = pcall(require, "obscure.palette." .. type_pallete)
 
-  -- local ans = vim.tbl_deep_extend("keep", palette or {})
-
   return palette
 end
 
