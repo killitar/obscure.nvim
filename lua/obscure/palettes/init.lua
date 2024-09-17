d59439815846364a675e005714420c4880388cd5
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-09-17 18:32:41 +0300

    feat(palletes): add type class

--- lua/obscure/palettes/init.lua
+++ lua/obscure/palettes/init.lua
@@ -1,7 +1,9 @@
 local M = {}
 
+---@class ColorScheme: Palette
 function M.get_palette(palette)
   local type_palette = palette or "obscure"
+
   local _, color_palette = pcall(require, "obscure.palettes." .. type_palette)
 
   return color_palette
